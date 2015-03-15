9d7ad675983aa4a46617816489714515bb271af4
Justin Love <git@JustinLove.name>
2015-03-15 18:05:50 -0500

    proof of concept displaying with vkboard

---
+++ README.md
@@ -0,0 +1,7 @@
+# Keyboard Viewer
+
+Draw keyboard in settings so you can tell what is mapped and what is free.
+
+## Libraries
+
+[vkeyboard.js](http://www.codeproject.com/Articles/13748/JavaScript-Virtual-Keyboard) is governed by the [CPOL](http://www.codeproject.com/info/cpol10.aspx)
---
+++ ui/mods/keyboard_viewer/keyboard_viewer.js
@@ -0,0 +1,82 @@
+(function() {
+  var vkboard_callback = function(char, id) {}
+  var vkboard_norm
+
+  var makeKeyboard = function(id) {
+    return new VKeyboard(id,   // container's id, mandatory
+                  vkboard_callback,// reference to callback function, mandatory
+                                // (this & following parameters are optional)
+                  true,         // create the arrow keys or not?
+                  true,         // create up and down arrow keys?
+                  false,        // reserved
+                  true,         // create the numpad or not?
+                  "",           // font name ("" == system default)
+                  "14px",       // font size in px
+                  "#FFF",       // font color
+                  "#F00",       // font color for the dead keys
+                  "#0D0D0D",       // keyboard base background color
+                  "#222",       // keys' background color
+                  "#CCC",       // background color of switched/selected item
+                  "#777",       // border color
+                  "#444",       // border/font color of "inactive" key
+                                // (key with no value/disabled)
+                  "#111",       // background color of "inactive" key
+                                // (key with no value/disabled)
+                  "#F77",       // border color of language selector's cell
+                  false,         // show key flash on click? (false by default)
+                  "#CC3300",    // font color during flash
+                  "#FF9966",    // key background color during flash
+                  "#CC3300",    // key border color during flash
+                  true,        // embed VKeyboard into the page?
+                  true,         // use 1-pixel gap between the keys?
+                  0);           // index (0-based) of the initial layout
+  }
+
+  model.settingGroups().push("keyview");
+  model.settingDefinitions().keyview = {title:"KeyView",settings:{}};
+  $.get('coui://ui/mods/keyboard_viewer/keyboard_viewer.html', function(html) {
+    var $section = $(html)
+    $(".container_settings").append($section)
+    ko.applyBindings(model, $section[0])
+
+    var boards = {
+      'normal': [],
+      'ctrl': [],
+      'alt': [],
+      'shift': [],
+    }
+    var setKeys = ko.computed(function() {
+      var keys = model.keyboardSettingsItems().map(function(item) {
+        return item.value()
+      })
+      keys.forEach(function(combo) {
+        if (combo == '') return
+        var parts = combo.split('+')
+        var key = parts.pop()
+        var board = parts.join('+')
+        if (board == '') board = 'normal'
+        if (board == 'shift+ctrl') board = 'ctrl+shift'
+        boards[board] = boards[board] || []
+        boards[board].push(key)
+      })
+      return keys
+    })
+
+    console.log(boards)
+
+    var $keyboards = $('.kbv_keyboards')
+    for(var prefix in boards) {
+      var id = 'vkeyboard_' + prefix.replace('+', '_')
+      var $kb = $('<div class="sub-group-title">' + prefix + '</div><div id="' + id + '"></div>')
+      console.log($kb)
+      $keyboards.append($kb)
+      var vkb = makeKeyboard(id)
+      var sk = boards[prefix]
+      $(vkb.Cntr).find('div div div div').filter(function() {
+        return sk.indexOf($(this).text().toLowerCase()) != -1
+      }).css('background-color', '#088')
+    }
+  })
+
+
+})()
